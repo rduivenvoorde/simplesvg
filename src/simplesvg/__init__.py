8e3caed93027badd944bc7603a168a4d2c8862da
Richard Duivenvoorde <richard@duif.net>
2014-01-08 17:14:02 +0100

    as uploaded to plugins.qgis.org as 0.8.4

--- src/simplesvg/__init__.py
+++ src/simplesvg/__init__.py
@@ -24,7 +24,7 @@ def name():
 def description():
   return "Create simple SVG from current view, editable with Inkscape"
 def version():
-  return "Version 0.8.3"
+  return "Version 0.8.4"
 def author():
     return "Richard Duivenvoorde"
 def email():
